67d17a541e618ffe1154888c55693c1dba2ff9e6
Denis Ulate <92893770+DenisUlate@users.noreply.github.com>
2025-12-16 09:31:44 -0600

    feat: Add challenge 16, "Packing Gifts for Santa", including problem instructions and JavaScript solution.

---
+++ challenge#16-packing-gifts-for-santa/challenge#16-main.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} gifts - The gifts to pack
+ * @param {number} maxWeight - The maximum weight of the sleigh
+ * @returns {number | null} The number of sleighs needed
+ * Return null if no sleigh can carry all the gifts
+ */
+function packGifts(gifts, maxWeight) {
+	if (gifts.length === 0) return 0;
+
+	if (gifts.some((gift) => gift > maxWeight)) {
+		return null;
+	}
+
+	let sleighs = 1;
+	let currentWeight = 0;
+
+	for (const gift of gifts) {
+		if (currentWeight + gift <= maxWeight) {
+			currentWeight += gift;
+		} else {
+			sleighs++;
+			currentWeight = gift;
+		}
+	}
+	return sleighs;
+}
